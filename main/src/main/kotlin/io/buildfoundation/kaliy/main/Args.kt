1cb07d32baa61cee1f093166d82e040ad43d998b
Vladimir Tagakov <vladimir@tagakov.com>
2019-06-14 00:31:23 -0700

    Add command line arg parsing

--- main/src/main/kotlin/io/buildfoundation/kaliy/main/Args.kt
+++ main/src/main/kotlin/io/buildfoundation/kaliy/main/Args.kt
@@ -1,11 +1,23 @@
 package io.buildfoundation.kaliy.main
 
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.SystemExitException
 import java.io.File
 
 internal data class Args(
         val configFile: File
 )
 
+
 internal fun parseArgs(rawArgs: Array<String>): Args {
-    return Args()
+    class Parser(parser: ArgParser) {
+        val config by parser.storing(help = "Path to the config file", names = *arrayOf("--config")) { File(this) }
+    }
+
+    try {
+        val parser = Parser(ArgParser(rawArgs))
+        return Args(parser.config)
+    } catch (e: SystemExitException) {
+        e.printAndExit()
+    }
 }
